232210d7ad489dd12a4f0760c75ec464cfbca226
Joyce Cho <joyjoycho@gmail.com>
2018-09-13 19:08:55 -0700

    complete linkedLIst and tree

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,46 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    // must create variable to ensure it will point to the same node
+    var newNode = Node(value)
+
+    // if list head has nothing
+    if (list.head === null) {
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      // save the tail node
+      var temp = list.tail;
+      // assign the newNode to the previous tail
+      temp.next = newNode;
+    }
+
+    // save the tail and move it to next
+
+    list.tail = newNode;
   };
 
   list.removeHead = function() {
+    var removed = list.head.value;
+    if (list.head !== null) {
+      list.head = list.head.next;
+    }
+
+    return removed;
   };
 
   list.contains = function(target) {
+    var currentNode = list.head;
+
+    while (currentNode !== null) {
+      if (currentNode.value === target) {
+        return true;
+      } else {
+        currentNode = currentNode.next;
+      }
+    }
+
+    return false;
   };
 
   return list;
@@ -27,3 +61,36 @@ var Node = function(value) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+// linkedList.addToTail(4);
+
+// { list:
+//     head: {
+//       value: 4
+//       next: null,
+//     }
+//     tail: {
+//       value: 4,
+//       next: null,
+//     }
+// }
+
+// linkedList.addToTail(5);
+// {
+//   list:
+//   head:
+//   {
+//     value: 4,
+//     next:
+//     {
+//       value: 5,
+//       next: null
+//
+//     }
+//   tail:
+//     {
+//       value: 5,
+//       next: null,
+//     }
+//   }
+// }
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,19 +2,39 @@ var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
 
-  // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
+
+  // extends the newTree methods from treeMethods
+  _.extend(newTree, treeMethods)
 
   return newTree;
 };
 
 var treeMethods = {};
 
+
+
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+
+  if (this.value === target) {
+    return true;
+  }
+
+  for (let i = 0; i < this.children.length; i++) {
+    var current = this.children[i];
+
+    if(current.contains(target)) {
+      return true;
+    }
+  }
+
+  return false;
 };
+
 
 
 
