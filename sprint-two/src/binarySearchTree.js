500364ce3597b43ec17c2edc55b4e2d89d53f025
Joyce Cho <joyjoycho@gmail.com>
2018-09-18 18:05:51 -0700

    complete binarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,53 @@
 var BinarySearchTree = function(value) {
+  let newTree = Object.create(treeMethods);
+
+  newTree.value = value;
+
+  return newTree;
 };
+
+var treeMethods = {};
+
+// must use es5???
+treeMethods.insert = function (value) {
+  var direction = this.value < value ? 'right' : 'left';
+
+  if(this[direction] === undefined) {
+    this[direction] = BinarySearchTree(value);
+  } else {
+    this[direction].insert(value);
+  }
+}
+
+treeMethods.contains = function (target) {
+  var direction = '';
+
+  if (this.value === target) {
+    return true;
+  }
+
+  this.value > target ? direction = 'left' : direction = 'right';
+
+  if (this[direction] === undefined) {
+    return false;
+  } else {
+    return this[direction].contains(target);
+  }
+}
+
+treeMethods.depthFirstLog = function (cb) {
+  cb(this.value);
+
+  if (this.left !== undefined) {
+    this.left.depthFirstLog(cb);
+  }
+
+  if (this.right !== undefined) {
+    this.right.depthFirstLog(cb);
+  }
+}
+
+
 
 
 /*
