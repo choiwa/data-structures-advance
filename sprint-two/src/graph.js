4050c024f2b47c42da1027088d5ee72bb2216e2a
Joyce Cho <joyjoycho@gmail.com>
2018-10-08 21:02:40 -0700

    complete callback

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -60,6 +60,9 @@ Graph.prototype.removeEdge = function(fromNode, toNode) {
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  for (let node in this._nodes) {
+    cb(node);
+  }
 };
 
 /*
